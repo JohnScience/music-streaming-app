943f3dd35054fc10036b26c229b946bf75e6e356
Evgeny Savonkin <evgenysavonkin@gmail.com>
2023-12-03 01:04:04 +0300

    fix: Refactor AudioRecordingController by transfering all the business logic into ServiceAudioRecordingsImpl class

--- back/src/main/java/com/music_streaming_app/controller/AudioRecordingController.java
+++ back/src/main/java/com/music_streaming_app/controller/AudioRecordingController.java
@@ -7,17 +7,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.sql.Blob;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,7 +23,6 @@ public class AudioRecordingController {
 
     private final ServiceAudioRecordingsImpl serviceAudioRecordingsImpl;
 
-    // Запись данных в БД
     @PostMapping(value = "/save", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<String> saveAudioRecording(@ModelAttribute DtoAudioRecording dtoAudioRecording) {
 
@@ -40,70 +32,22 @@ public class AudioRecordingController {
         return ResponseEntity.ok("Audio recording " + saveInDateBase);
     }
 
-    // Получаем файл в виде потока
     @GetMapping("/{id}")
     public ResponseEntity<StreamingResponseBody> getAudioRecording(@PathVariable Long id) {
 
-        // Получаем файловые данные из сервиса
         Optional<AudioRecording> audioRecordingOptional = serviceAudioRecordingsImpl.getAudioRecordingById(id);
 
-        // Обрабатываем случай, когда аудиозапись не найдена
         if (audioRecordingOptional.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
 
-        Blob audioBlob = audioRecordingOptional.get().getAudioBlob();
-
-        // Создаем объект StreamingResponseBody для потоковой передачи данных
-        StreamingResponseBody responseBody = outputStream -> {
-            // Получаем поток байтов из объекта Blob
-            try (InputStream inputStream = audioBlob.getBinaryStream()) {
-                byte[] buffer = new byte[4096];
-                int bytesRead;
-                while ((bytesRead = inputStream.read(buffer)) != -1) {
-                    // Записываем байты в выходной поток
-                    outputStream.write(buffer, 0, bytesRead);
-                }
-                outputStream.flush();
-            } catch (SQLException | IOException e) {
-                throw new RuntimeException(e);
-            }
-        };
-
-        // Устанавливаем заголовки ответа для указания типа контента и длины файла
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-        try {
-            headers.setContentLength((int) audioBlob.length());
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        // Возвращаем StreamingResponseBody в ответе
-        return ResponseEntity.ok()
-                .headers(headers)
-                .body(responseBody);
+        //Чтобы не дергать бд несколько раз, то уже полученный объект передаём в метод
+        return serviceAudioRecordingsImpl.getAudioRecording(audioRecordingOptional);
     }
 
-    // Получаем все записи из БД
     @GetMapping("/all")
     public ResponseEntity<List<DtoAudioRecording>> getAllAudioRecordings() {
-        List<AudioRecording> allAudioRecordings = serviceAudioRecordingsImpl.getAllAudioRecordings();
-
-        List<DtoAudioRecording> dtoAudioRecordings = new ArrayList<>();
-
-        for (AudioRecording audioRecording : allAudioRecordings) {
-            DtoAudioRecording dtoAudioRecording = DtoAudioRecording.builder()
-                    .id(audioRecording.getId())
-                    .artist(audioRecording.getArtist())
-                    .description(audioRecording.getDescription())
-                    .sourceUrl(audioRecording.getSourceUrl())
-                    .build();
-
-            dtoAudioRecordings.add(dtoAudioRecording);
-        }
-
-        return ResponseEntity.ok(dtoAudioRecordings);
+        return serviceAudioRecordingsImpl.getAllDtoAudioRecordings();
     }
 
     @Operation(summary = "Получаем рекомендуемые записи из БД")
--- back/src/main/java/com/music_streaming_app/service/impl/ServiceAudioRecordingsImpl.java
+++ back/src/main/java/com/music_streaming_app/service/impl/ServiceAudioRecordingsImpl.java
@@ -5,12 +5,18 @@ import com.music_streaming_app.entity.AudioRecording;
 import com.music_streaming_app.repository.RepositoryAudioRecordings;
 import com.music_streaming_app.service.ServiceAudioRecordings;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 import javax.sql.rowset.serial.SerialBlob;
 import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Blob;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -44,6 +50,7 @@ public class ServiceAudioRecordingsImpl implements ServiceAudioRecordings {
         try {
             audioRecording = AudioRecording.builder()
                     .audioBlob(new SerialBlob(dtoAudioRecording.getFile().getBytes()))
+                    .artist(dtoAudioRecording.getArtist())
 //                    .author(dtoAudioRecording.getArtist())
                     .description(dtoAudioRecording.getDescription())
                     .sourceUrl(dtoAudioRecording.getSourceUrl()).build();
@@ -53,5 +60,51 @@ public class ServiceAudioRecordingsImpl implements ServiceAudioRecordings {
 
         repositoryAudioRecordings.save(audioRecording);
         return true;
+    }
+
+    public ResponseEntity<StreamingResponseBody> getAudioRecording(Optional<AudioRecording> audioRecordingOptional) {
+        Blob audioBlob = audioRecordingOptional.get().getAudioBlob();
+        StreamingResponseBody responseBody = outputStream -> {
+            try (InputStream inputStream = audioBlob.getBinaryStream()) {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+                outputStream.flush();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        };
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+
+        try {
+            headers.setContentLength((int) audioBlob.length());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(responseBody);
+    }
+
+    public ResponseEntity<List<DtoAudioRecording>> getAllDtoAudioRecordings() {
+        List<DtoAudioRecording> dtoAudioRecordings = new ArrayList<>();
+
+        for (AudioRecording audioRecording : getAllAudioRecordings()) {
+            DtoAudioRecording dtoAudioRecording = DtoAudioRecording.builder()
+                    .id(audioRecording.getId())
+                    .artist(audioRecording.getArtist())
+                    .description(audioRecording.getDescription())
+                    .sourceUrl(audioRecording.getSourceUrl())
+                    .build();
+
+            dtoAudioRecordings.add(dtoAudioRecording);
+        }
+
+        return ResponseEntity.ok(dtoAudioRecordings);
     }
 }
