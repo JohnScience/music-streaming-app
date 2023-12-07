9809cdfbd7fc9074a70ef764fd6ffa5f556547f3
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-12-08 00:00:01 +0300

    fix: fix teg on component Information

--- front/src/components/pages/about/Information.tsx
+++ front/src/components/pages/about/Information.tsx
@@ -6,40 +6,39 @@ const Information: React.FC = () => {
     <div>
       <List>
         <Item>
-          <span> Документация по проекту в Google </span>
+          <Text> Документация по проекту в Google </Text>
           <Link href='https://drive.google.com/drive/folders/1LMDWBQJvWTMkXAgWi2CtlUQaBiRl8ZKY'>
             https://drive.google.com/drive/folders/1LMDWBQJvWTMkXAgWi2CtlUQaBiRl8ZKY
           </Link>
         </Item>
         <Item>
-          <span>Документация по проекту в ClickUp </span>
+          <Text>Документация по проекту в ClickUp </Text>
           <Link href='https://app.clickup.com/9005175548/v/dc/8cc04qw-288/8cc04qw-308'>
             https://app.clickup.com/9005175548/v/dc/8cc04qw-288/8cc04qw-308
           </Link>
         </Item>
         <Item>
-          <span> Макет: </span>
+          <Text> Макет: </Text>
           <Link href=' https://www.figma.com/file/m9mPk1duxN3yVPc8EinbOu/Project-Music-player?type=design&node-id=0%3A1&mode=design&t=M3kiGVCFLk3BlFYN-1'>
             https://www.figma.com/file/m9mPk1duxN3yVPc8EinbOu/Project-Music-player?type=design&node-id=0%3A1&mode=design&t=M3kiGVCFLk3BlFYN-1
           </Link>
         </Item>
         <Item>
-          <span> Репозиторий проекта </span>{' '}
+          <Text> Репозиторий проекта </Text>
           <Link href='https://github.com/JohnScience/music-streaming-app'>
             https://github.com/JohnScience/music-streaming-app
           </Link>
         </Item>
         <Item>
-          {' '}
-          <span>Таск Менеджер проекта CLickUp </span>
+          <Text>Таск Менеджер проекта CLickUp </Text>
           <Link href='https://app.clickup.com/9005175548/home'>https://app.clickup.com/9005175548/home</Link>
         </Item>
         <Item>
-          <span>Документация по API (требуется запустить приложение) </span>
+          <Text>Документация по API (требуется запустить приложение) </Text>
           <Link href=' http://localhost:8080/swagger-ui/index.html'> http://localhost:8080/swagger-ui/index.html</Link>
         </Item>
         <Item>
-          <span> Важно! Прочитайте Устав проекта.</span>
+          <Text> Важно! Прочитайте Устав проекта.</Text>
         </Item>
       </List>
     </div>
