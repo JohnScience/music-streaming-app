c85b292b033d8fb0ce95648a5aef5c5908ff48d4
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-12-08 13:53:12 +0300

    fix: fix global styles and styles MainLayout

--- front/src/layouts/MainLayout.tsx
+++ front/src/layouts/MainLayout.tsx
@@ -8,21 +8,29 @@ import RightMenu from '../components/pages/home/RightMenu'
 
 const MainLayout: React.FC = () => {
   return (
-    <>
+    <ContentWrapper>
       <MainMenu />
       <MainContent>
         <Outlet />
       </MainContent>
       <RightMenu />
       <Player />
-    </>
+    </ContentWrapper>
   )
 }
 
 export default MainLayout
 
 const MainContent = styled.main`
-  main {
-    padding-top: 20px;
-  }
+  padding-top: 20px;
+`
+
+const ContentWrapper = styled.div`
+  margin: auto;
+  max-width: 1440px;
+  display: grid;
+  grid-template-columns: 275px 1fr 348px;
+  grid-template-rows: 1fr auto;
+  column-gap: 30px;
+  justify-content: center;
 `
--- front/src/theme/globalStyles.js
+++ front/src/theme/globalStyles.js
@@ -8,17 +8,7 @@ const GlobalStyle = createGlobalStyle`
     background:#111111;
     font-family: Open-Sans, Helvetica, Sans-Serif;
   }
-  
-  #root{
-    margin:auto;
-    max-width:1440px;
-    display:grid;
-    grid-template-columns: 275px 1fr 348px ;
-    grid-template-rows: 1fr auto;
-    column-gap:30px;
-    justify-content:center;
-  }
-  
+
   
 `
 
