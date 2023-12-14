bb5b93590aeb493a90adf55136dbf8d16bb06783
Anastasia <kukina.anastasia.dev@gmail.ru>
2023-12-14 22:15:55 +0300

    добавила отдельный элемент трека, useState оставила в плейлисте

--- front/src/components/pages/home/PlayList.tsx
+++ front/src/components/pages/home/PlayList.tsx
@@ -1,57 +1,13 @@
 import { useState } from "react";
-import Pausa from "../home/assets/Pausa.svg"
-import Play from "../home/assets/Play.svg"
-import Playing from "../home/assets/Playing.svg"
 import Image1 from "../home/assets/1.jpg"
 import Image2 from  "../home/assets/2.jpg"
 import Image3 from  "../home/assets/3.jpg"
-import "./PlayList.css"
 import React from "react";
 import { styled }  from "styled-components"
+import Treck from "./Treck";
+ 
 
-const Li = styled.li`
-display: flex;   
-background-color: #252020;
-width: 100%;
-color: ${({$invalid})=> $invalid ? `#9c088e` : `#bab6b6`}; 
-max-width: 900px;
-margin: 1rem auto;
-justify-content: space-around; 
-border:${({$invalid})=> $invalid ? `1px solid #212121` : `1px solid #212121`};
-border-radius: 5px;
-align-items: center ;
-box-shadow:${({$invalid})=> $invalid ? `0 0 5px 2px #E283D2` : `none`}  ;
-$img {
-  border-radius: 50%;
-      height: 3rem;
-    box-shadow: 0 0 2px 2px #9c088e;
-}
-`
-const Button = styled.button`
-background-color: #e03e2500; 
-    border: none; 
-    &:hover{
-      cursor: pointer;
-    }
-    
-`
 
-const Title = styled.div`
-display: flex;
-    justify-content: flex-start;
-    width: 40%;
-`
-const Image = styled.img`
-border-radius: 50%;
-      height: 3rem;
-    box-shadow: 0 0 2px 2px #9c088e;
-`
-const ButtonPlay = styled.div`
-display: flex;
-`
-const ButtonPausa = styled.div`
-padding-right: 2rem;
-`
 const  musicList = [
     {
       id: 1,
@@ -95,41 +51,21 @@ const  musicList = [
           
 
         return(
-            musicList.map((item, index)=>(
-              <ul >
-                <Li $invalid = {activeItem === index}
-                >
-                  <div >
-                        <h3>{item.id}</h3>
-                    </div>
-                    
-                        <img className="component_image" src={item.image} />
-                    
-                    <Title >
-                        <p>{item.title + " - " + item.artist}</p>
-                    </Title>
-                    <div >
-                        {Math.floor(item.time / 60) + ':' + item.time % 60}
-                    </div>
-                    <div>
-                        {activeItem === index && isPlaying ? (
-                            <ButtonPlay>
-                                <ButtonPausa >
-                                 <img src={Playing} />
-                                </ButtonPausa>
-                                <Button  onClick={()=> handlePausa()} >
-                             <img src={Pausa} width="22" height="22" />
-                          </Button>
-                           </ButtonPlay>
-                        ) : (
-                          <Button  onClick={()=> handlePlay(index)} >
-                             <img src={Play} width="22" height="22" />
-                          </Button>
-                        )
-                         }
-                    </div>
-                </Li>
-                </ul>
-            ))
+          <ul>
+            {musicList.map((item)=>(
+              <Treck 
+              id={item.id}
+              artist={item.artist}
+              time={item.time}
+              title={item.title}
+              image={item.image}
+              onPlay={handlePlay}
+              onPausa={handlePausa}
+              activeItem={activeItem}
+              isPlaying={isPlaying}
+              />
+             
+            ))}
+          </ul>
         )
     }
---
+++ front/src/components/pages/home/Treck.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { styled }  from "styled-components"
+import Pausa from "../home/assets/Pausa.svg"
+import Play from "../home/assets/Play.svg"
+import Playing from "../home/assets/Playing.svg"
+import { useState } from "react";
+
+const Li = styled.li`
+display: flex;   
+background-color: #252020;
+width: 100%;
+color: ${({$invalid})=> $invalid ? `#9c088e` : `#bab6b6`}; 
+max-width: 900px;
+margin: 1rem auto;
+justify-content: space-around; 
+border:${({$invalid})=> $invalid ? `1px solid #212121` : `1px solid #212121`};
+border-radius: 5px;
+align-items: center ;
+box-shadow:${({$invalid})=> $invalid ? `0 0 5px 2px #E283D2` : `none`}  ;
+    
+   
+`
+
+const Button = styled.button`
+background-color: #e03e2500; 
+    border: none; 
+    &:hover{
+      cursor: pointer;
+    }
+    
+`
+
+const Title = styled.div`
+display: flex;
+    justify-content: flex-start;
+    width: 40%;
+`
+const Image = styled.div`
+img{
+border-radius: 50%;
+      height: 3rem;
+    box-shadow: 0 0 2px 2px #9c088e;
+  }
+`
+const ButtonPlay = styled.div`
+display: flex;
+`
+const ButtonPausa = styled.div`
+padding-right: 2rem;
+`
+
+
+ export default function Treck ( {onPausa, id, time,title,artist, image, onPlay, activeItem, isPlaying}) {
+
+    
+    return(
+        <Li key={id} 
+        $invalid = {activeItem === id}>
+                  <div >
+                        <h3>{id}</h3>
+                    </div>
+                    
+                    <Image>
+                        <img  src={image} />
+                    </Image>
+                    <Title >
+                        <p>{title + " - " + artist}</p>
+                    </Title>
+                    <div >
+                        {Math.floor(time / 60) + ':' + time % 60}
+                    </div>
+                    <div>
+                        {activeItem === id && isPlaying ? (
+                            <ButtonPlay>
+                                <ButtonPausa >
+                                 <img src={Playing} />
+                                </ButtonPausa>
+                                <Button  onClick={()=>onPausa()} >
+                             <img src={Pausa} width="22" height="22" />
+                          </Button>
+                           </ButtonPlay>
+                        ) : (
+                          <Button  onClick={()=>onPlay(id)} >
+                             <img src={Play} width="22" height="22" />
+                          </Button>
+                        )
+                         }
+                    </div>
+                </Li>
+            
+    )
+ }
